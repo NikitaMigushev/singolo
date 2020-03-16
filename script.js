f008c4d48d26d5a2ad58aa0e5479fd69663b1ab9
NikitaMigushev <migushev@gmail.com>
2020-03-16 22:55:01 +0300

    feat: add interactive menu

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+var activeclass = document.querySelectorAll('.menuli');
+for (var i = 0; i < activeclass.length; i++) {
+  activeclass[i].addEventListener('click', activateClass);
+}
 
+function activateClass(e) {
+  for (var i = 0; i < activeclass.length; i++) {
+    activeclass[i].classList.remove('active');
+  }
+  e.target.classList.add('active');
+}
